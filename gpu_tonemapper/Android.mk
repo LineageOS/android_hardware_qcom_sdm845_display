c7a039ce2e4fd7762a742e1ef072448c74b4b296
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2021-01-16 19:27:04 +0100

    remove headers
    Change-Id: Ic5ef7c6d96cfa96494b22ea9c538fa3b12abf353

--- gpu_tonemapper/Android.mk
+++ gpu_tonemapper/Android.mk
@@ -2,10 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 
 include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO     := $(common_header_export_path)
-LOCAL_COPY_HEADERS        := TonemapFactory.h Tonemapper.h
 LOCAL_VENDOR_MODULE       := true
-include $(BUILD_COPY_HEADERS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE              := libgpu_tonemapper
